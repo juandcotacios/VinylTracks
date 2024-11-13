89807343d1d956956db587eace6c24ecf0ca894b
Juan David Cotacio Sánchez <juand.cotacios@konradlorenz.edu.co>
2024-11-13 13:55:10 -0500

    Creación de la view de Register,Login,Compras y creación token de user
    Se creó una view para cada uno de estos nuevos campos  para facilitar la adición de datos y se importaron librerías de framework para las views y encriptar la contraseña del usuario.

--- VinylTracks/api/views.py
+++ VinylTracks/api/views.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets
+from django.contrib.auth import authenticate
+from rest_framework import viewsets, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Producto, Compra, Proveedor
-from .serializers import ProductoSerializer, CompraSerializer, ProveedorSerializer
+from rest_framework.authtoken.models import Token
+from .models import Producto, Venta, Proveedor
+from .serializers import ProductoSerializer, VentaSerializer, ProveedorSerializer, UserSerializer
 # Create your views here.
 
 class ProveedorViewSet(viewsets.ModelViewSet):
@@ -17,5 +20,31 @@ class CompraViewSet(viewsets.ModelViewSet):
     serializer_class = CompraSerializer
 
     def perform_create(self, serializer):
-   
+
         serializer.save()
+        
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+class VentaViewSet(viewsets.ModelViewSet):
+    queryset = Venta.objects.all()
+    serializer_class = VentaSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(usuario=self.request.user)   
