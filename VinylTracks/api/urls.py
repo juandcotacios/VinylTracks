2e520646fefb6d7e461b4ae08f09d5d218b17f4e
Juan David Cotacio Sánchez <juand.cotacios@konradlorenz.edu.co>
2024-11-13 14:12:29 -0500

    Endpoints de register y login e instalación de las apps
    Se añadieron los endpoints a urls.py y se instalaron las apps en el main app para la implementación.

--- VinylTracks/api/urls.py
+++ VinylTracks/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductoViewSet, CompraViewSet, ProveedorViewSet
+from .views import ProductoViewSet, CompraViewSet, ProveedorViewSet, RegisterView, LoginView
 
 router = DefaultRouter()
 router.register(r'productos', ProductoViewSet)
@@ -8,5 +8,7 @@ router.register(r'compras', CompraViewSet)
 router.register(r'proveedores', ProveedorViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)), 
+    path('', include(router.urls)),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),  
 ]
