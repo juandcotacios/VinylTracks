f9ef37ae5e8c313df44dfadfca04f6b502161ed0
Laura Zoriano <laurazoriano.22@gmail.com>
2024-11-17 21:09:01 -0500

    Refactorización: Renombrar aplicación proveedores a mainweb y mover archivo HTML
    Se renombra la aplicación "proveedores" a "mainweb" para reflejar la nueva lógica central de la aplicación, que funcionará como la página principal del proyecto. Además, se mueve el archivo indexWebUser.html a la nueva estructura de carpetas bajo templates/mainweb. Los cambios incluyen la actualización del archivo settings.py para reflejar el nuevo nombre en INSTALLED_APPS, la modificación del archivo apps.py para cambiar el nombre de la configuración de la aplicación, y la reorganización de la plantilla correspondiente.

--- VinylTracks/mainweb/apps.py
+++ VinylTracks/mainweb/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProveedoresConfig(AppConfig):
+class MainWebConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'proveedores'
+    name = 'mainweb'
