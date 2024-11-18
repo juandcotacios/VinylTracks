7301a547d7fc3e72b80b26556e1d16b1abbaecb8
SebastianSC <sebassandoc@gmail.com>
2024-11-17 23:36:57 -0500

    Scripts API Register y Login
    Generacion de metodos para conectar el frontend con el API url de login y register

--- VinylTracks/mainweb/static/js/main.js
+++ VinylTracks/mainweb/static/js/main.js
@@ -3,3 +3,25 @@ document.querySelectorAll('.add-to-cart').forEach(button => {
         alert('Producto añadido al carrito');
     });
 });
+
+function loginFunc() {
+    fetch('api/login/')
+    .then(Response => Response.json)
+    .then(data => {
+        window.location.href = '/api/login/';
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+function RegisterFunc() {
+    fetch('api/register/')
+    .then(Response => Response.json)
+    .then(data => {
+        window.location.href = '/api/register/';
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
